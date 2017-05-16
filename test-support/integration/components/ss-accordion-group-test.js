b686e240d5189651df859edca95cc872dbdff222
Aaron Hansen <aaronbhansen@gmail.com>
2017-05-15 21:11:07 -0600

    Adding accordion tests

--- test-support/integration/components/ss-accordion-group-test.js
+++ test-support/integration/components/ss-accordion-group-test.js
@@ -5,21 +5,186 @@ moduleForComponent('ss-accordion-group', 'Integration | Component | ss accordion
   integration: true
 });
 
+
 test('it renders', function(assert) {
+  assert.expect(2);
 
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
-  this.render(hbs`{{ss-accordion-group}}`);
-
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
   this.render(hbs`
-    {{#ss-accordion-group}}
-      template block text
+    {{#ss-accordion-group class="styled" duration=0 as |ss|}}
+      {{#ss.accordion isOpen=true as |ss|}}
+        {{#ss.title}}
+          Semantic UI
+        {{/ss.title}}
+        {{#ss.content}}
+          Accordion Component
+        {{/ss.content}}
+      {{/ss.accordion}}
+      {{#ss.accordion as |ss|}}
+        {{#ss.title}}
+          Title 2
+        {{/ss.title}}
+        {{#ss.content}}
+          Content 2
+        {{/ss.content}}
+      {{/ss.accordion}}
     {{/ss-accordion-group}}
   `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  // Test default state
+  assert.equal(this.$('.ui.accordion').length, 1);
+  assert.equal(this.$('.ui.accordion .active').length, 2);
+});
+
+test('clicking activates title', function(assert) {
+  assert.expect(4);
+
+  this.render(hbs`
+    {{#ss-accordion-group class="styled" duration=0 as |ss|}}
+      {{#ss.accordion as |ss|}}
+        {{#ss.title class="one"}}
+          Semantic UI
+        {{/ss.title}}
+        {{#ss.content class="one"}}
+          Accordion Component
+        {{/ss.content}}
+      {{/ss.accordion}}
+      {{#ss.accordion as |ss|}}
+        {{#ss.title class="two"}}
+          Title 2
+        {{/ss.title}}
+        {{#ss.content class="two"}}
+          Content 2
+        {{/ss.content}}
+      {{/ss.accordion}}
+    {{/ss-accordion-group}}
+  `);
+
+  assert.equal(this.$('.ui.accordion .one.title.active').length, 0);
+  assert.equal(this.$('.ui.accordion .one.content.active').length, 0);
+
+  // Test clicking activates accordion
+  this.$('.ui.accordion .one.title').click();
+  assert.equal(this.$('.ui.accordion .one.title.active').length, 1);
+  assert.equal(this.$('.ui.accordion .one.content.active').length, 1);
+});
+
+test('dynamically added content is clickable', function(assert) {
+  assert.expect(7);
+
+  this.set('panes', Ember.A([]));
+
+  this.render(hbs`
+    {{#ss-accordion-group class="styled" duration=0 as |ss|}}
+      {{#ss.accordion as |ss|}}
+        {{#ss.title class="one"}}
+          Semantic UI
+        {{/ss.title}}
+        {{#ss.content class="one"}}
+          Accordion Component
+        {{/ss.content}}
+      {{/ss.accordion}}
+      {{#ss.accordion as |ss|}}
+        {{#ss.title class="two"}}
+          Title 2
+        {{/ss.title}}
+        {{#ss.content class="two"}}
+          Content 2
+        {{/ss.content}}
+      {{/ss.accordion}}
+      {{#each panes as |pane|}}
+        {{#ss.accordion as |ss|}}
+          {{#ss.title data-test-extra-title=pane}}
+            Extra Section {{pane}}
+          {{/ss.title}}
+          {{#ss.content data-test-extra-content=pane}}
+            Extra Content {{pane}}
+          {{/ss.content}}
+        {{/ss.accordion}}
+      {{/each}}
+    {{/ss-accordion-group}}
+  `);
+
+  assert.equal(this.$('.ui.accordion').length, 1);
+  assert.equal(this.$('.ui.accordion .title').length, 2);
+  assert.equal(this.$('.ui.accordion .content').length, 2);
+
+  Ember.run(() => {
+    this.get('panes').pushObjects([1,2]);
+  });
+
+  assert.equal(this.$('.ui.accordion .title').length, 4);
+  assert.equal(this.$('.ui.accordion .content').length, 4);
+
+  // Test clicking activates accordion
+  this.$('.ui.accordion [data-test-extra-title=1]').click();
+  assert.equal(this.$('.ui.accordion [data-test-extra-title=1].active').length, 1);
+  assert.equal(this.$('.ui.accordion .active').length, 2);
+});
+
+test('exclusive false allows more than one active title', function(assert) {
+  assert.expect(4);
+
+  this.render(hbs`
+    {{#ss-accordion-group class="styled" duration=0 as |ss|}}
+      {{#ss.accordion as |ss|}}
+        {{#ss.title class="one"}}
+          Semantic UI
+        {{/ss.title}}
+        {{#ss.content class="one"}}
+          Accordion Component
+        {{/ss.content}}
+      {{/ss.accordion}}
+      {{#ss.accordion as |ss|}}
+        {{#ss.title class="two"}}
+          Title 2
+        {{/ss.title}}
+        {{#ss.content class="two"}}
+          Content 2
+        {{/ss.content}}
+      {{/ss.accordion}}
+    {{/ss-accordion-group}}
+  `);
+
+  // Test clicking activates accordion
+  this.$('.ui.accordion .two.title').click();
+  assert.equal(this.$('.ui.accordion .two.active').length, 2);
+  assert.equal(this.$('.ui.accordion .active').length, 2);
+
+  this.$('.ui.accordion .one.title').click();
+  assert.equal(this.$('.ui.accordion .one.active').length, 2);
+  assert.equal(this.$('.ui.accordion .active').length, 2);
+});
+
+test('collapsible false allows doesnt allow active to close', function(assert) {
+  assert.expect(4);
+
+  this.render(hbs`
+    {{#ss-accordion-group class="styled" duration=0 collapsible=false as |ss|}}
+      {{#ss.accordion as |ss|}}
+        {{#ss.title class="one"}}
+          Semantic UI
+        {{/ss.title}}
+        {{#ss.content class="one"}}
+          Accordion Component
+        {{/ss.content}}
+      {{/ss.accordion}}
+      {{#ss.accordion as |ss|}}
+        {{#ss.title class="two"}}
+          Title 2
+        {{/ss.title}}
+        {{#ss.content class="two"}}
+          Content 2
+        {{/ss.content}}
+      {{/ss.accordion}}
+    {{/ss-accordion-group}}
+  `);
+
+  assert.equal(this.$('.ui.accordion .active').length, 0);
+  // Test clicking activates accordion
+  this.$('.ui.accordion .one.title').click();
+  assert.equal(this.$('.ui.accordion .one.active').length, 2);
+  assert.equal(this.$('.ui.accordion .active').length, 2);
+
+  this.$('.ui.accordion .one.title').click();
+  assert.equal(this.$('.ui.accordion .active').length, 2);
 });
--- test-support/integration/components/ss-accordion-test.js
+++ test-support/integration/components/ss-accordion-test.js
@@ -6,20 +6,43 @@ moduleForComponent('ss-accordion', 'Integration | Component | ss accordion', {
 });
 
 test('it renders', function(assert) {
+  assert.expect(2);
 
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
-  this.render(hbs`{{ss-accordion}}`);
-
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
   this.render(hbs`
-    {{#ss-accordion}}
-      template block text
+    {{#ss-accordion class="styled" as |ss|}}
+      {{#ss.title}}
+        Semantic UI
+      {{/ss.title}}
+      {{#ss.content}}
+        Accordion Component
+      {{/ss.content}}
     {{/ss-accordion}}
   `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  // Test default state
+  assert.equal(this.$('.ui.accordion').length, 1);
+  assert.equal(this.$('.ui.accordion .active').length, 0);
+});
+
+test('clicking activates title', function(assert) {
+  assert.expect(4);
+
+  this.render(hbs`
+    {{#ss-accordion class="styled" as |ss|}}
+      {{#ss.title}}
+        Semantic UI
+      {{/ss.title}}
+      {{#ss.content}}
+        Accordion Component
+      {{/ss.content}}
+    {{/ss-accordion}}
+  `);
+
+  assert.equal(this.$('.ui.accordion .title.active').length, 0);
+  assert.equal(this.$('.ui.accordion .content.active').length, 0);
+
+  // Test clicking activates accordion
+  this.$('.ui.accordion .title').click();
+  assert.equal(this.$('.ui.accordion .title.active').length, 1);
+  assert.equal(this.$('.ui.accordion .content.active').length, 1);
 });
